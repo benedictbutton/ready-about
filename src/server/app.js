2b4a4cfe46ae2c381675dd4ad70ee98928b45930
benedictbutton <benedictdaly@hotmail.com>
2019-12-23 17:04:29 -0500

    root address

--- src/server/app.js
+++ src/server/app.js
@@ -1,47 +1,46 @@
-"use strict";
-const mongoose = require("mongoose");
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const dotenv = require("dotenv");
-const errorHandler = require("errorhandler");
-const passport = require("passport");
-const scheduler = require("./config/scheduler");
+const mongoose = require('mongoose');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const errorHandler = require('errorhandler');
+const passport = require('passport');
+const scheduler = require('./config/scheduler');
 
 // initiate app
 const app = express();
 
 // configure app
 dotenv.config();
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-const isDev = process.env.NODE_ENV || "development";
+const isDev = process.env.NODE_ENV || 'development';
 
 const port = process.env.PORT || process[2] || 8080;
 
-const FileStore = require("session-file-store")(session);
+const FileStore = require('session-file-store')(session);
 
 app.use(
   session({
     store: new FileStore(),
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: false
-  })
+    saveUninitialized: false,
+  }),
 );
 
 // Models
-require("./models/user");
+require('./models/user');
 // require("./config/passport");
-require("./config/passport")(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // const User = require("./models/user");
 app.use(passport.initialize());
@@ -50,13 +49,13 @@ app.use(passport.session());
 mongoose.connect(
   process.env.DATABASE_URL,
   {
-    useNewUrlParser: true
+    useNewUrlParser: true,
   },
   (err, database) => {
     if (err) return console.log(err);
     //   const db = database;
     //   require("./routes")(db);
-  }
+  },
 );
 
 // configure Mongoose
@@ -66,10 +65,10 @@ mongoose.connect(
 //     useNewUrlParser: true
 //   }
 // );
-mongoose.set("debug", true);
+mongoose.set('debug', true);
 
 // process requests to api/auth with routes/auth.js file
-app.use("/api", require("./routes/index"));
+app.use('/api', require('./routes/index'));
 
 // default route
 // app.get("/*", function(req, res) {
@@ -81,20 +80,31 @@ app.use("/api", require("./routes/index"));
 // });
 
 if (isDev) {
-  const webpack = require("webpack");
-  const webpackMiddleware = require("webpack-dev-middleware");
-  const webpackConfig = require("../../webpack.config.js");
+  const webpack = require('webpack');
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackConfig = require('../../webpack.config.js');
   app.use(errorHandler());
   app.use(
     webpackMiddleware(webpack(webpackConfig), {
-      publicPath: "/",
-      status: { colors: true }
-    })
+      publicPath: '/',
+      status: { colors: true },
+    }),
   );
 } else {
-  app.use(express.static("dist"));
+  app.use(express.static('dist'));
 }
 
-app.listen(port, () => console.log("Ready."));
+app.get('/*', function(req, res) {
+  res.sendFile(
+    path.join(__dirname, '../../dist/index.html'),
+    function(err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    },
+  );
+});
+
+app.listen(port, () => console.log('Ready.'));
 
 scheduler.start();
