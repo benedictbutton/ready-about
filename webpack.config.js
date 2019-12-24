31cfd0a0739d58f509f137b6ec7a925de04b3d81
benedictbutton <benedictdaly@hotmail.com>
2019-12-23 19:51:01 -0500

    mode none var

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
+const path = require('path');
 
-const distDir = path.resolve(__dirname, "dist");
-const webpack = require("webpack");
+const distDir = path.resolve(__dirname, 'dist');
+const webpack = require('webpack');
 // Make sure this plugin is listed first
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/client/index.js"
+    index: './src/client/index.js',
   },
   output: {
     path: distDir,
-    publicPath: "/",
-    filename: "[name].js"
+    publicPath: '/',
+    filename: '[name].js',
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"]
+    extensions: ['*', '.js', '.jsx'],
   },
   devServer: {
     port: 5000,
@@ -24,26 +24,27 @@ module.exports = {
     hot: true,
     historyApiFallback: true,
     proxy: {
-      "/api": "http://localhost:8080"
-    }
+      '/api': 'http://localhost:8080',
+    },
   },
-  devtool: "cheap-module-eval-source-map",
+  devtool: 'cheap-module-eval-source-map',
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         options: {
+          cache: true,
           emitWarning: true,
-          configFile: "./.eslintrc.json"
-        }
+          configFile: './.eslintrc.json',
+        },
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader',
       },
       {
         test: /\.(css)$/,
@@ -53,44 +54,43 @@ module.exports = {
             options: {
               // you can specify a publicPath here
               // by default it uses publicPath in webpackOptions.output
-              publicPath: "/",
-              hmr: process.env.NODE_ENV === "development"
-            }
+              publicPath: '/',
+              hmr: process.env.NODE_ENV === 'development',
+            },
           },
           {
-            loader: "css-loader" // translates CSS into CommonJS modules
+            loader: 'css-loader', // translates CSS into CommonJS modules
           },
           {
-            loader: "postcss-loader", // Run postcss actions
+            loader: 'postcss-loader', // Run postcss actions
             options: {
               plugins() {
                 // postcss plugins, can be exported to postcss.config.js
-                return [require("autoprefixer")];
-              }
-            }
-          }
-        ]
+                return [require('autoprefixer')];
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg|jpe?g|pdf)$/,
-        loader: "url-loader?limit=100000"
-      }
-    ]
+        loader: 'url-loader?limit=100000',
+      },
+    ],
   },
-  mode: "none",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Ready About",
-      template: "./src/client/template.html"
+      title: 'Ready About',
+      template: './src/client/template.html',
     }),
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
