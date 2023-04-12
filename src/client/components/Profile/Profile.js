b299dd5705b4fd9be7dce9f282e606d98362646b
benedict button <benedictdaly@hotmail.com>
2023-04-12 18:58:35 -0400

    browser alias error fix

--- src/client/components/Profile/Profile.js
+++ src/client/components/Profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import SignOut from '../Authentication/SignOut';
+import SignOut from '../Authentication/Signout';
 // material-ui
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
